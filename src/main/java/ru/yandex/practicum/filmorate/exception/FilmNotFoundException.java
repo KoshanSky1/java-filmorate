743b1371efa9850d9c00c2de16d5e856bac08c4a
Galina Lobacheva <147919859+KoshanSky1@users.noreply.github.com>
2024-03-17 23:15:44 +0300

    Update FilmNotFoundException.java

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
@@ -2,6 +2,6 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class FilmNotFoundException extends RuntimeException {
     public FilmNotFoundException(String message) {
-       super(message);
+        super(message);
     }
 }
